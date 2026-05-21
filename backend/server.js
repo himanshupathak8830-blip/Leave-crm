44e98237d9ff850d2b2e9a689be06e1483f515e0
Himanshu <himanshupathak8830@gmail.com>
2026-05-21 18:50:59 +0530

    fic
    fix

--- backend/server.js
+++ backend/server.js
@@ -58,10 +58,10 @@ app.use((req, res, next) => {
 
 app.use(express.json({ limit: "20kb" }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
 const loginAttempts = new Map();
